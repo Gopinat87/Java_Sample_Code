5f7313c9707dfb29fa4b6bc30ce0ecb42851316b
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-05-26 19:36:40 +0530

    Add files via upload

---
+++ LFT/sc.java
@@ -0,0 +1,9 @@
+public class sc{
+    public static void main(String[] args){
+        int a = 15;
+        int b = 10;
+        System.out.println( (a>20) && (a<30) );  // short circuit operator 
+        System.out.println( (b>5) || (b<20) );  // same Short circuit operator // used to reduce time complexity
+    }
+}
+
