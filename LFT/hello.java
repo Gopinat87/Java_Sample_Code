8c2ba836eb83718b8c2c3d503012bb418d97225a
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-05-26 19:38:26 +0530

    Add files via upload

---
+++ LFT/hello.java
@@ -0,0 +1,11 @@
+public class hello{
+    public static void main(String args[]){
+        int variablee ;
+        variablee = 600;
+        char nameer = 'g';
+        String my = "gopinath";
+        System.out.println("Introduction to Java Programming " + variablee);
+        System.out.println(nameer);
+        System.out.print(my);
+    }
+}
