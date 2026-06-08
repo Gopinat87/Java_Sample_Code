c280b72f1a3140fba102f9adb300107d65399a7b
Gopinath U <124049217+Gopinat87@users.noreply.github.com>
2026-06-08 08:43:23 +0530

    Add files via upload

---
+++ LFT/class_objects/g1.java
@@ -0,0 +1,17 @@
+class games {
+    int free_fire;
+    int pubg;
+    int Call_of_duty;
+}
+
+public class g1 {
+    public static void main(String[] args) {
+        games Battleground = new games() ;
+        Battleground.free_fire = 50 ;
+        Battleground.pubg = 100;
+        Battleground.Call_of_duty = 100;
+
+        System.out.println(Battleground.Call_of_duty);
+        
+    }
+}
